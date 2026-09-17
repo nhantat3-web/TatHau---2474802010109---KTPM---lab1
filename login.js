67a6d1338834bfb8de77ef4c7068ebdf5322bf75
nhantat3-web <nhantat3@gmail.com>
2026-09-17 16:24:01 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 function login(username, password) {
-    return user === "admin" && pass === "123";
+    return username === "admin" && password === "123";
 }
 
 // Chỉ xử lý form khi chạy trên trình duyệt
